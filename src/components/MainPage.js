909fc3c042875952b4f0b08037ec74fb2abcc6b5
choizinyoung <ligt69209@gmail.com>
2022-10-06 11:44:08 +0900

    데이터베이스연결

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,68 +1,67 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./MainPage.css";
 import axios from "axios";
+import dayjs from "dayjs";
+import realativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(realativeTime);
+
 const MainPage = () => {
   let [products, setProducts] = React.useState([]);
-
   useEffect(() => {
     axios
-      .get(
-        "https://3404fbff-960c-489f-83bd-c90b36470130.mock.pstmn.io/products"
-      )
+      .get("http://localhost:8080/products/")
       .then((res) => {
-        products = res.data.products;
+        products = res.data.Product;
+        console.log(res);
         setProducts(products);
       })
       .catch((err) => {
         return console.log(err);
       });
   }, []);
+  if(products === undefined) {
+    return <h1>상품정보를 받고있습니다.</h1>
+  }
   return (
     <>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" alt="logo" />
-        </div>
-      </div>
       <div id="body">
         <div id="banner">
-          <img src="images/banners/banner1.png" alt="banner" />
+          <img src="images/banners/banner1.png" alt="" />
         </div>
         <h2>Products</h2>
         <div id="product-list">
-          {products.map((product,idx)=>{
+          {products.map((product, idx) => {
             return (
               <div className="product-card" key={idx}>
-              <div>
-                <img
-                  className="product-img"
-                  src={product.imageUrl}
-                  alt={product.key}
-                />
+                <Link className="product-link" to={`/product/${product.id}`}>
+                  <div>
+                    <img
+                      className="product-img"
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                  </div>
+                  <div className="product-content">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div class="product-footer">
+                      <div className="product-seller">
+                        <img
+                          className="product-avatar"
+                          src="images/icons/avatar.png"
+                          alt=""
+                        />
+                        <span>내추럴코어</span>
+                      </div>
+                      <span className="product-dete">{dayjs(product.createdAt).fromNow()}</span>
+                    </div>
+                  </div>
+                </Link>
               </div>
-              <div className="product-content">
-                <span className="product-name">{product.name}</span>
-                <span className="product-price">{product.price}원</span>
-                <div className="product-seller">
-                  <img
-                    className="product-avatar"
-                    src="images/icons/avatar.png"
-                    alt="avatar"
-                  />
-                  <span>{product.seller}</span>
-                </div>
-              </div>
-            </div>  
-            )
+            );
           })}
         </div>
-      </div>
-      <div id="footer">
-        <a href="#">회사소개</a>
-        <a href="#">이용약관</a>
-        <a href="#">통신판매업:123-1234</a>
-        <a href="#">사업자등록번호:456-4567</a>
-        <a href="#">개인정보...</a>
       </div>
     </>
   );
