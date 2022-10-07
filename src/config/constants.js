e6e8493805c7d394d5c8c3f98e3f681cf3323439
choizinyoung <ligt69209@gmail.com>
2022-10-07 15:57:57 +0900

    api 주소 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
 //constants.js
-export const API_URL = "http://localhost:8080";
+/*export const API_URL = "http://localhost:8080";*/
+export const API_URL = "https://mango-shop-server1.herokuapp.com";
