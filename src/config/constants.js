9c56ab80876be10f2a5305bd946a14bd0b2f9875
choizinyoung <ligt69209@gmail.com>
2022-12-08 16:44:20 +0900

    서버배포호스팅수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 //constants.js
 //export const API_URL = "http://localhost:8080";
 //export const API_URL = "https://mango-shop-server1.herokuapp.com";
-export const API_URL = process.env.NODE_ENV === 'production'? "https://react-app-surver.herokuapp.com" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === 'production'? "https://react-server.onrender.com" : "http://localhost:8080";
